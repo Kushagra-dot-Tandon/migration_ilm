42f65305c693a9386a52289621ee5738eda92d14
kushagra-dot-tandon <kushagra.tandon@maplelabs.com>
2022-03-31 14:44:24 +0530

    update: cleaning

--- multidb/app/tests.py
+++ multidb/app/tests.py
@@ -1,15 +1,15 @@
-from django.test import TestCase
-from .models import *
+# from django.test import TestCase
+# from .models import *
 
-# Create your tests here.
-data_table_1 = Datasource.objects.using('snappyflow').get(id='eoUq7Z83w0')
-print("Printing ")
-print(data_table_1.datasource_name)
+# # Create your tests here.
+# data_table_1 = Datasource.objects.using('snappyflow').get(id='eoUq7Z83w0')
+# print("Printing ")
+# print(data_table_1.datasource_name)
 
-data_table_2 = DatasourceClone.objects.create(
-    id=data_table_1.id,
-    datasource_name=data_table_1.datasource_name,
-    type=data_table_1.type,
-    default_ds=data_table_1.default_ds
-)
-data_table_2.save(using='local')
+# data_table_2 = DatasourceClone.objects.create(
+#     id=data_table_1.id,
+#     datasource_name=data_table_1.datasource_name,
+#     type=data_table_1.type,
+#     default_ds=data_table_1.default_ds
+# )
+# data_table_2.save(using='local')
